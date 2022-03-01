24208baf9250e66b0c75f052a1608b1ad378fed7
Daniel Mikusa <dmikusa@vmware.com>
2022-03-01 16:18:22 -0500

    Bump to using Go 1.17 and fix module declarations to have major version suffix
    Signed-off-by: Daniel Mikusa <dmikusa@vmware.com>

--- tomcat/home_test.go
+++ tomcat/home_test.go
@@ -27,7 +27,7 @@ import (
 	"github.com/paketo-buildpacks/libpak"
 	"github.com/sclevine/spec"
 
-	"github.com/paketo-buildpacks/apache-tomcat/tomcat"
+	"github.com/paketo-buildpacks/apache-tomcat/v7/tomcat"
 )
 
 func testHome(t *testing.T, context spec.G, it spec.S) {
